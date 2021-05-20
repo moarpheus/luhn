b479ba1434e5ff4e6dc4a5aef2d1da58b25ac201
Juris Gribuska <juris.gribuska@bt.com>
2021-05-20 09:10:55 +0100

    ugly pass

--- luhn.rb
+++ luhn.rb
@@ -1,8 +1,23 @@
-=begin
-Write your code for the 'Luhn' exercise in this file. Make the tests in
-`luhn_test.rb` pass.
+require 'byebug'
 
-To get started with TDD, see the `README.md` file in your
-`ruby/luhn` directory.
-=end
-
+class Luhn
+  def self.valid? number
+    clean_number = number.gsub(' ', '').reverse
+    return false if clean_number.size <= 1 || clean_number.match(/\A[\d[:blank:]]+\z/).nil?
+    double = clean_number
+              .split('')
+              .each_slice(2)
+              .map(&:last)
+              .map do |n| 
+                r = n.to_i * 2
+                r > 9? r -9 : r
+              end
+              .inject(:+)
+    single = clean_number
+              .split('')
+              .each_slice(2)
+              .map {|n| n.first.to_i}
+              .inject(:+)
+    ((double + single) % 10 == 0)
+  end
+end
